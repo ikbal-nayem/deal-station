95ca1f25e20ff4c4a073b65bb68c6d4d357d7e4e
Ikbal Nayem <ikbalnayem000@gmail.com>
2025-12-30 05:33:01 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@/

---
+++ src/components/layout/Logo.tsx
@@ -0,0 +1,17 @@
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Logo() {
+  return (
+    <Link href="/" className="flex items-center gap-2 text-xl font-bold font-headline px-2">
+        <Image
+          src="/logo.png"
+          alt="The Deal Station Logo"
+          width={40}
+          height={40}
+          className='dark:invert'
+        />
+    </Link>
+  );
+}
